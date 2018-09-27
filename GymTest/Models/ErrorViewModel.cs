23a31bc1a7716dd46d2fc225f2327dd374eb6d59
sebas <spcolonna@gmail.com>
2018-09-26 21:15:10 -0300

    testcommit

---
+++ GymTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GymTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
