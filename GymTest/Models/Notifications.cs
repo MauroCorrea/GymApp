c5dd03b13256b68edcf9a3a32900cee4c5ec0a3e
spcolonna <spcolonna@gmail.com>
2019-03-15 21:40:56 -0300

    noti

---
+++ GymTest/Models/Notifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymTest.Models
+{
+    public class Notification
+    {
+        public int NotificationId { get; set; }
+            
+        public bool Everyone { get; set; }
+
+        public bool Send { get; set; }
+
+        public string To { get; set; }
+
+        public string Message { get; set; }
+    }
+}
