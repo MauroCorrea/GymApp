dd59167b965c9f88924461d5ef57ba727ee8f32b
Mauro Correa <mauro.correa1988@gmail.com>
2019-06-04 10:11:39 -0300

    +Fix generacion de reporte movimientos de caja +Ajuste send mail

--- GymTest/Services/ISendEmail.cs
+++ GymTest/Services/ISendEmail.cs
@@ -5,5 +5,8 @@ namespace GymTest.Services
     public interface ISendEmail
     {
         void SendEmail(Dictionary<string, string> bodyData, string templateName, string subject, List<string> receipts, List<string> filePathAttachment = null);
+
+        void SendEmailRegister(Dictionary<string, string> bodyData, string templateName, string subject, List<string> receipts);
+
     }
 }
